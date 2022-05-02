67a69dbeea93b5e619ebde62217882b99fad2ba5
ethank2222 <59261562+ethank2222@users.noreply.github.com>
2022-05-02 13:49:45 -0700

    Create Frontend

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById("write").addEventListener("click", function(){
+    console.log(document.getElementById("text").value);
+});
+document.getElementById("read").addEventListener("click", function(){
+
+});
